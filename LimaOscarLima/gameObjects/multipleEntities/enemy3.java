91e1a4f4dac96abebda4382b4c2e806a239110b8
Renan-MouraN <156025786+Renan-MouraN@users.noreply.github.com>
2024-06-30 19:37:46 -0300

    Delete LimaOscarLima directory

--- LimaOscarLima/gameObjects/multipleEntities/enemy3.java
+++
@@ -1,8 +0,0 @@
-package LimaOscarLima.gameObjects.multipleEntities;
-
-public class enemy3 extends enemy1{
-
-    public enemy3(double enemies_radius, long enemiesSpawnTime) {
-        super(enemies_radius, enemiesSpawnTime);
-    }
-}
